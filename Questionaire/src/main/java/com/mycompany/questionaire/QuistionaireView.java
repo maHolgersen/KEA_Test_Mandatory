122325907d25055c8b4cfa51bb9d4d0ec4c707b1
maHolgersen <ma.holgersen@gmail.com>
2018-10-31 16:04:07 +0100

    its a start

--- Questionaire/src/main/java/com/mycompany/questionaire/QuistionaireView.java
+++ Questionaire/src/main/java/com/mycompany/questionaire/QuistionaireView.java
@@ -7,10 +7,13 @@ package com.mycompany.questionaire;
 
 import java.awt.FlowLayout;
 import java.awt.Frame;
-import java.awt.GridLayout;
 import java.awt.Label;
 import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JRadioButton;
 /**
@@ -19,17 +22,40 @@ import javax.swing.JRadioButton;
  */
 public final class QuistionaireView extends Frame{
     private JFrame pnl;
+    private Questionaire questionaire;
+    private JButton submit = new JButton("Submit");
+    private List radioGrps;
+    
+    
     
     public QuistionaireView(Questionaire questionaire){
+        this.questionaire = questionaire;
         pnl = new JFrame();
         pnl.setLayout(new FlowLayout());
+        radioGrps = new ArrayList();
+        
         for (int i = 0; i < questionaire.getQuestions().size(); i++) {
             Panel questionPnl = new Panel(new FlowLayout());
             questionPnl.setSize(200, 200);
             createQuesion((Question)questionaire.getQuestions().get(i), questionPnl);
         }
         pnl.setVisible(true);
+        
+        submit.addActionListener((ActionEvent e) -> {
+            for (int i = 0; i < pnl.getComponents().length; i++) {
+                
+                ButtonGroup btngrp = (ButtonGroup) radioGrps.get(i);
+            }
+            //for (Object question : questionaire.getQuestions()) {
+            //    int score = 0;
+                
+            //}    
+        });
+        
+        pnl.add(submit);
     }
+    
+    
     
     public void createQuesion(Question question, Panel questionPnl){
         
@@ -46,8 +72,11 @@ public final class QuistionaireView extends Frame{
            radioGrp[i] = new JRadioButton((String)question.getAnswers().get(i));
            btnGrp.add(radioGrp[i]);
            questionPnl.add(radioGrp[i]);
+            System.out.println(radioGrp[i]);
         }
         
+        radioGrps.add(btnGrp);
         questionPnl.setVisible(true);
+        
     }
 }
